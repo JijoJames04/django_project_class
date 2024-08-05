77a0fb4663d004b216a1909399b6fb3af81f0626
Jijo James <jijojames9495@gmail.com>
2024-08-05 22:01:58 +0530

    Managing Static Files & handling forms

--- movies/views.py
+++ movies/views.py
@@ -2,11 +2,39 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def create(request):
-    return render(request,'create.html')
+    if request.POST:
+        print(request.POST.get('title'))
+        print(request.POST.get('year'))
+        
+    return render(request, 'create.html')
+
 
 def list(request):
-    return render(request,'list.html')
+    movie_data = {
+        'movies': [
+            {
+                'title': 'Godfather',
+                'year': 1990,
+                'summary': 'story of an underworld king',
+                'success': True,
+                'img': 'godfather.jpeg'
+            }, {
+                'title': 'Titanic',
+                'year': 1990,
+                'success': True,
+                'img': 'titanic.jpeg'
+            }, {
+                'title': 'Into the wild',
+                'year': 1980,
+                'summary': 'story of an underworld king',
+                'success': True,
+                'img': 'wild.jpeg'
+            }]
+    }
+    return render(request, 'list.html', movie_data)
+
 
 def edit(request):
-    return render(request,'edit.html')
+    return render(request, 'edit.html')
